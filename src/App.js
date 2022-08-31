7a0d278a2d9e01f9499134ec631365901d7da595
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-08-31 16:45:24 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default function App(){
+
+//   const Alunos = [ 
+//     { nome:'Letícia',cor:'Vermelho' },
+//     { nome:'Paula', cor:'Rosa' }, 
+//     { nome:'Raquel', cor:'Azul' }
+//   ]
+
+//   return(
+//     < >
+//       <ul>{Alunos.map((item) => (
+//         <li>{item.nome} {item.cor}</li>
+//       ))}</ul>
+//     </>
+//   )
+// }
+
+// ######################################
+
+// import React,{useState} from 'react';
+
+// export default function App() {
+ 
+//   const [ users, setUsers ] = useState([
+//   { name:"Raquel", color:'blue' },
+//   { name:"Paula", color:'pink' },
+//   { name:"David", color:'yellow' },
+//   { name:"Marlon", color:'red' }
+// ])
+
+
+// function Change(){
+//   setUsers([
+//     { name:"Raquel", color:'blue' },
+//     { name:"Paula", color:'pink' },
+//     { name:"David", color:'aquamarine' },
+//     { name:"Marlon", color:'red'} 
+// ])
+// }
+
+//   return (
+//     < >
+//     <ul>{users.map( item => (
+//       <li>{item.name} {item.color}</li>
+//     ))}</ul>
+
+//     <button onClick={() =>{Change()}}>Change</button>
+//     </>
+//   );
+// }
+
+// ######################################
+
+// import React, {useState} from 'react'
+
+// export default function App(){
+
+//   const [ input, setInput ] = useState()
+
+//   function Buy(){
+
+//   } 
+
+//   return(
+//     < >
+//     <input value={input} onChange={(event) =>{setInput(event.target.value)}}/>
+//     <h1>WELCOME TO ISA'S WEBPAGE</h1>
+//     <button onClick={() =>{}}>Buy</button>
+//     <p>Thanks {input}</p>
+//     </>
+//   )
+// }
+
+// ######################################
+
+import React, {useState} from 'react'
+
+export default function ToDo(){
+
+const [ input, setInput ] = useState()
+const [ taskList, setTaskList ] = useState([])
+
+const Add = () =>{
+
+  
 }
 
-export default App;
+  return(
+    <form onSubmit={(e) => {e.preventDefault()}}>
+      <button onClick={() => {Add()}}>ADD</button>
+      <input value={input} onChange={e => setInput(e.target.value)} />
+    </form>
+  )
+}
